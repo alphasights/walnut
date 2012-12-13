5c3f3f0da046d1d1228b0f10ead0769c80a6de0d
Tadas Tamosauskas <tadas@pdfcv.com>
2012-12-13 10:16:03 +0000

    Fix syntax warnings

--- services.js
+++ services.js
@@ -5,7 +5,7 @@ Service({
   status: function(response) {
     return response["status"]["Production"].toLowerCase() == "green";
   }
-})
+});
 
 Service({
   name: 'Heroku Tools',
@@ -14,7 +14,7 @@ Service({
   status: function(response) {
     return response["status"]["Development"].toLowerCase() == "green";
   }
-})
+});
 
 Service({
   name: 'EC2 N.Virginia',
@@ -24,23 +24,23 @@ Service({
     var text = td.text();
     return (text == "Service is operating normally." || text.match(/(RESOLVED)/).length > 0);
   }
-})
+});
 
 Service({
   name: 'MongoHQ Rose',
   url: 'http://status.mongohq.com/api/v1/services/rose/events',
   status: function(response) {
-    return JSON.parse(response)["events"].length == 0
+    return JSON.parse(response)["events"].length === 0;
   }
-})
+});
 
 Service({
   name: 'Pusher',
   url: 'http://status.pusherapp.com/',
   status: function(response) {
-    return $(response).find('#answer').hasClass('up')
+    return $(response).find('#answer').hasClass('up');
   }
-})
+});
 
 Service({
   name: 'Rackspace Email',
@@ -49,7 +49,7 @@ Service({
   status: function(response) {
     return $(response).find('#current #current-group-3 .field-group-message').text().match(/normally/).length > 0;
   }
-})
+});
 
 Service({
   name: 'Github',
@@ -58,31 +58,31 @@ Service({
   status: function(response) {
     return response["status"] != "majorproblem" && response["status"] != "minorproblem";
   }
-})
+});
 
 Service({
   name: 'CloudMailin',
   url: 'http://cloudmailin-status.herokuapp.com/',
   status: function(response) {
     var status = response["status"];
-    return  (status == "up" || status == "reduced_capacity")
+    return  (status == "up" || status == "reduced_capacity");
   }
-})
+});
 
 // http://tubeupdates.com/documentation/
 Service({
   name: 'Jubilee Line',
-  url: 'http://api.tubeupdates.com/\?method\=get.status\&lines\=jubilee',
+  url: "http://api.tubeupdates.com/?method=get.status&lines=jubilee",
   status: function(response) {
     var status = response["response"]["lines"][0]["status"];
-    return status == "good service"
+    return status == "good service";
   }
-})
+});
 
 Service({
   name: 'Exceptional',
   url: 'http://status.getexceptional.com/',
   status: function(response) {
-    return $(response).find("#Currently strong").first().html() < 5000
+    return $(response).find("#Currently strong").first().html() < 5000;
   }
-})
+});
